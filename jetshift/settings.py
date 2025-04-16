3ddf562718aaf4a4792516c945af7ef66b607912
Md Obydullah <hi@obydul.me>
2025-04-16 22:58:34 +0600

    add home page

--- jetshift/settings.py
+++ jetshift/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'jetshift.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'jetshift', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- jetshift/urls.py
+++ jetshift/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home_view
 
 urlpatterns = [
+    path('', home_view),
     path('api/', include('app.urls')),
 ]
 
